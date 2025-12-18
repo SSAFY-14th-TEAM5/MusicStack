f07ee7c8fbcc8e212cc3196e009ee9716ae34caf
mskim-99 <kms99400@gmail.com>
2025-12-18 15:03:56 +0900

    feat: create login and signup views

--- Frontend/musicstack-project/src/router/index.js
+++ Frontend/musicstack-project/src/router/index.js
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/components/Home.vue'
+import LogInView from '@/views/LogInView.vue'
+import SignUpView from '@/views/SignUpView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component : Home
+    },
+    {
+      path: '/login',
+      name: 'LogInView',
+      component : LogInView
+    },
+    {
+      path: '/signup',
+      name: 'SignUpView',
+      component : SignUpView
+    },
+  ],
 })
 
 export default router
---
+++ Frontend/musicstack-project/src/views/SignUpView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>SignUp Page</h1>
+    <form @submit.prevent="signUp">
+      <label for="username">username: </label>
+      <input type="text" id="username" v-model.trim="username">
+      <br>
+      <label for="password1">password: </label>
+      <input type="password" id="password1" v-model.trim="password1">
+      <br>
+      <label for="password2">password confirmation: </label>
+      <input type="password" id="password2" v-model.trim="password2">
+      <br>
+      <label for="nickname">nickname: </label>
+      <input type="text" id="nickname" v-model.trim="nickname">
+      <br>
+      <input type="submit" value="signup">
+    </form>
+  </div>
+</template>
+
+<script setup>
+  import { useAccountStore } from '@/stores/accounts'
+  import { ref } from 'vue'
+
+  const accountStore = useAccountStore()
+
+  const username = ref(null)
+  const password1 = ref(null)
+  const password2 = ref(null)
+  const nickname = ref(null)
+
+  const signUp = function () {
+    const payload = {
+      username: username.value,
+      password1: password1.value,
+      password2: password2.value,
+      nickname: nickname.value
+    }
+    accountStore.signUp(payload)
+    
+  }
+
+</script>
+
+<style scoped>
+
+</style>
